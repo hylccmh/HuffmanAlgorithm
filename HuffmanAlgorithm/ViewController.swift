e5e9c4d96e35b4568f3ff70d743afa49ca1e4f28
huyunlong <894457665@qq.com>
2019-01-31 10:27:35 +0800

    Initial Commit

---
+++ HuffmanAlgorithm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuffmanAlgorithm
+//
+//  Created by huyunlong on 2019/1/31.
+//  Copyright © 2019 huyunlong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
